bda86c78dbe7810cba9d626df664ac323270ca0c
jesusthedev <104586580+jesusthedev@users.noreply.github.com>
2022-05-11 11:48:58 -0500

    added code

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,46 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id.includes(id));
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((lastA,lastB) => 
+    lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1 );
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  let result = 0;
+  const booksBorrowedByAccount = books.forEach((book) => {
+    if (!!book.borrows) {
+      book.borrows.forEach((accounts) => {
+        if (accounts.id === account.id) {
+          result++;
+        }
+      });
+    }
+  });
+  return result;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function _getAuthor(book, authors) {
+  const author = authors.find((author) => author.id === book.authorId);
+  return author;
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const borrowedBooks = [];
+  books.forEach((book) => {
+    let bookBorrows = book.borrows;
+    bookBorrows.forEach((borrow) => {
+      if (borrow.id === account.id && !borrow.returned) {
+        borrowedBooks.push(book);
+      }
+    });
+  });
+  let result = borrowedBooks.map((book) => {
+    return { ...book, author: _getAuthor(book, authors) };
+  });
+  return result;
+}
 
 module.exports = {
   findAccountById,
