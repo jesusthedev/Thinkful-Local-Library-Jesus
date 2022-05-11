42a7f7632b2c2434ad031d0c86375eec119d4103
jesusthedev <104586580+jesusthedev@users.noreply.github.com>
2022-05-11 12:36:01 -0500

    added comments

--- public/src/books.js
+++ public/src/books.js
@@ -1,17 +1,21 @@
+//function that returns array of authors with ID
 function findAuthorById(authors, id) {
   return authors.find((author) => author.id === id );
 }
 
+//function that returns array of books with ID
 function findBookById(books, id) {
   return books.find((book) => book.id.includes(id));
 }
 
+//function that returns an array consisting of two arrays: an array of available books and an array of books currently borrowed
 function partitionBooksByBorrowedStatus(books) {
   const availableList = books.filter((book) => book.borrows[0].returned === true);
   const borrowedList = books.filter((book) => book.borrows[0].returned === false);
   return [borrowedList, availableList];
 }
 
+//function that returns an array of all of the transactions within a borrow array. the result includes account object information
 function getBorrowersForBook(book, accounts) {
   const result = [];
   for (let account of accounts) {
